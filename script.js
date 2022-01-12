092a03b3f11cca8c29ada1c43a6607f6c02f74cc
Hami <szymontok4rski@gmail.com>
2022-01-12 19:52:03 +0100

    dla agaty

---
+++ script.js
@@ -0,0 +1,8 @@
+const funct = () => {
+    
+    if(document.getElementById('wk').style.display == "block")
+    document.getElementById("wk").style.display = "none";
+    else
+    document.getElementById("wk").style.display = "block";
+
+   }
